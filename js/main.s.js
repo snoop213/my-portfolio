e913ba2b8ecc4c6a6e013ce508fc595bdb8ca2bc
snoop213 <81127474+snoop213@users.noreply.github.com>
2021-07-03 22:35:30 +0900

    Create main.s.js

---
+++ js/main.s.js
@@ -0,0 +1,101 @@
+'use strict';
+
+window.onload = function() {
+
+  let nomura = document.getElementById("nomura");
+  let btn = document.getElementById("btn");
+  let nomuraList = nomura.style;
+  btn.onclick = function(){
+    if( nomuraList.display === "block" ) {
+      nomuraList.display = "none";
+    } else {
+      nomuraList.display = "block";
+    }
+  };
+  
+  let ou = document.getElementById("ou");
+  let btn2 = document.getElementById("btn2");
+  let ouList = ou.style;
+  btn2.onclick = function(){
+    if( ouList.display === "block" ) {
+      ouList.display = "none";
+    } else {
+      ouList.display = "block";
+    }
+  };
+  let takagi = document.getElementById("takagi");
+  let btn3 = document.getElementById("btn3");
+  let takagiList = takagi.style;
+  btn3.onclick = function(){
+    if( takagiList.display === "block" ) {
+      takagiList.display = "none";
+    } else {
+      takagiList.display = "block";
+    }
+  };
+  let toyoda = document.getElementById("toyoda");
+  let btn4 = document.getElementById("btn4");
+  let toyodaList = toyoda.style;
+  btn4.onclick = function(){
+    if( toyodaList.display === "block" ) {
+      toyodaList.display = "none";
+    } else {
+      toyodaList.display = "block";
+    }
+  };
+  let nagasima = document.getElementById("nagasima");
+  let btn5 = document.getElementById("btn5");
+  let nagasimaList = nagasima.style;
+  btn5.onclick = function(){
+    if( nagasimaList.display === "block" ) {
+      nagasimaList.display = "none";
+    } else {
+      nagasimaList.display = "block";
+    }
+  };
+  let kadoda = document.getElementById("kadoda");
+  let btn6 = document.getElementById("btn6");
+  let kadodaList = kadoda.style;
+  btn6.onclick = function(){
+    if( kadodaList.display === "block" ) {
+      kadodaList.display = "none";
+    } else {
+      kadodaList.display = "block";
+    }
+  };
+  let hukumoto = document.getElementById("hukumoto");
+  let btn7 = document.getElementById("btn7");
+  let hukumotoList = hukumoto.style;
+  btn7.onclick = function(){
+    if( hukumotoList.display === "block" ) {
+      hukumotoList.display = "none";
+    } else {
+      hukumotoList.display = "block";
+    }
+  };
+  let harimoto = document.getElementById("harimoto");
+  let btn8 = document.getElementById("btn8");
+  let harimotoList = harimoto.style;
+  btn8.onclick = function(){
+    if( harimotoList.display === "block" ) {
+      harimotoList.display = "none";
+    } else {
+      harimotoList.display = "block";
+    }
+  };
+  let enatu = document.getElementById("enatu");
+  let btn9 = document.getElementById("btn9");
+  let enatuList = enatu.style;
+  btn9.onclick = function(){
+    if( enatuList.display === "block" ) {
+      enatuList.display = "none";
+    } else {
+      enatuList.display = "block";
+    }
+  };
+  
+   
+  
+ 
+  
+  }
