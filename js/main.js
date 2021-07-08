a1301e5373876a09738bd70d7ede939ac1be89f5
snoop213 <81127474+snoop213@users.noreply.github.com>
2021-07-08 13:46:53 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,31 +1,14 @@
 'use strict';
 
 window.onload = function() {
-  const bat1 = new Audio("金属バットで打つ1.mp3");
+  const bat = new Audio("金属バットで打つ1.mp3");
             
- document.getElementById("bat1").onclick = function(){
-  bat1.play();  
-            };
-  const bat2 = new Audio("金属バットで打つ1.mp3");
-            
- document.getElementById("bat2").onclick = function(){
-  bat2.play();  
-            };
-  const bat3 = new Audio("金属バットで打つ1.mp3");
-            
- document.getElementById("bat3").onclick = function(){
-  bat3.play();  
-            };
-  const bat4 = new Audio("金属バットで打つ1.mp3");
-            
- document.getElementById("bat4").onclick = function(){
-  bat4.play();  
-            };
-  const bat5 = new Audio("金属バットで打つ1.mp3");
-            
- document.getElementById("bat5").onclick = function(){
-  bat5.play();  
-            };
+ document.querySelectorAll("a").forEach(function(a){
 
- 
-  }
+   a.addEventListener('click',()=>{
+     bat.play();  
+ });
+
+
+  
+  });
